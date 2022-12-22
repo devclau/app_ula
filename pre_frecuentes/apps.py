96482cccfc060d6ddd728c8c00e278826697ff67
Claudio Q <116306876+devclau@users.noreply.github.com>
2022-12-22 12:49:13 -0300

    Delete pre_frecuentes directory

--- pre_frecuentes/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PreFrecuentesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pre_frecuentes'
-    verbose_name = "Preguntas Frecuentes"
-    
